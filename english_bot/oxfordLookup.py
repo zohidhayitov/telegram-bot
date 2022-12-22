619ba008f4da254d9fe6a3e8746befcac6aa227b
Zohid Hayitov <102049840+zohidhayitov@users.noreply.github.com>
2022-12-23 04:51:39 +0500

    Yangilangan fayl
    english_bot

---
+++ english_bot/oxfordLookup.py
@@ -0,0 +1,29 @@
+import requests
+
+app_id = "955b67ee"
+app_key = "26bf66e8b4404952fb9d177db9d18216"
+language = "en-gb"
+
+def getDefinitions(word_id):
+    url = "	https://od-api.oxforddictionaries.com/api/v2/entries/" + language + "/" + word_id.lower()
+    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+    res = r.json()
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+    definitions = []
+    for sense in senses:
+        definitions.append(f"👉 {sense['definitions'][0]}")
+    output['definitions'] = "\n".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == '__main__':
+    from pprint import pprint as print
+    print(getDefinitions('Great Britain'))
+    print(getDefinitions('america'))
